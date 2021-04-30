7a8f7b373aa2e34333d6549e2ff8edaedef54095
harshit shetty <hs1251999@gmail.com>
2021-04-30 15:16:54 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-"use strict"
-let userscore=0;
+let userScore=0;
 let computerScore=0;
 const userScore_span=document.getElementById("user_score");
 const computerScore_span=document.getElementById("computer_score");
@@ -9,55 +8,65 @@ const rock_div=document.getElementById('r');
 const paper_div=document.getElementById('p');
 const scissors_div=document.getElementById('s');
 
- rock_div.addEventListener ("click",function(){
-   game("r";)
-
- })
-
-function getComputerChoice["userchoice"]{
-  const Choices=["r" "p" "s"];
-  const randomNumber=Math.floor(math.random()*3);
-  return Choices[randomNumber];o
+function getComputerChoice () {
+  const Choices=['r','p','s'];
+  const randomNumber=Math.floor(math.random() *3);
+  return Choices[randomNumber];
 }
+console.log
 function convertToWord(letter){
   if( letter==="r")return "Rock";
   if(letter==="p")return "paper";
-  if(letter==="s")return "scisscors";
+  return "scissors";
 }
- function win(userChoice,computerChoice){
-   userscore++;
-   userscore_span.innerHTML=userscore;
-   computerscore_span.innerHTML=computerscore;
-result_p:innerHTML={`${convertToWord(userChoice)} ${user} beats ${ covertToWord(computerChoice)} ${comp}} you won`}
- }
+ function win(userChoice,computerChoice)
+ {
+   userScore++;
+   userScore_span.innerHTML=userScore;
+   computerScore_span.innerHTML=computerScore;
+   result_p.innerHTML=`${convertToWord(userChoice)} (user) beats ${ convertToWord(computerChoice)} (comp). you won`};
+ 
   function loss (userChoice,computerChoice){
-  userscore --;
-  userscore_span.innerHTML=userscore;
-  computerscore_span.innerHTML=computerscore;
-result_p:innerHTML={`${convertToWord(userChoice)} ${user} beats ${ covertToWord(computerChoice)} ${comp}} you lose`}
-  }
+  userScore --;
+  userScore_span.innerHTML=userScore;
+  computerScore_span.innerHTML=computerScore;
+result_p.innerHTML=`${convertToWord(userChoice)} (user) beats ${ convertToWord(computerChoice)} (comp). you lose`}
+  
   function draw(userChoice,computerChoice){
-  userscore_span.innerHTML=userscore;
-  computerscore_span.innerHTML=computerscore;
-result_p:innerHTML={`${convertToWord(userChoice)} ${user} beats ${ covertToWord(computerChoice)} ${comp}} draw match`}
-  }
+  userScore_span.innerHTML=userScore;
+  computerScore_span.innerHTML=computerScore;
+result_p.innerHTML=`${convertToWord(userChoice)}(user)  beats ${ convertToWord(computerChoice)} (comp). draw match`}
+  
 
-  function game(userChoice){
-    const computerChoice=getComputerChoice();
-    switch(userChoice=computerChoice){
-      case "rs":
-        case "pr":
-          case "sp":
-            win (userChoice,computerChoice);
-            break;
-            case "rp":
-              case "ps":
-                case "sr":
-                  loss (userChoice,computerChoice);
-                  break;
-                  case "rr":
-                    case "pp":
-                      case "ss":
-                        draw (userChoice,computerChoice);
-                        break;
-    }
+  function game(userChoice) {
+let computerChoice=getComputerChoice();
+switch(userChoice=computerChoice){
+case "rs":
+case "pr":
+case "sp":
+win (userChoice,computerChoice);
+break;
+case "rp":
+case "ps":
+case "sr":
+loss (userChoice,computerChoice);
+break;
+case "rr":
+case "pp":
+case "ss":
+draw (userChoice,computerChoice);
+break; 
+} 
+  }
+ function main() 
+ {rock_div.addEventListener('click',function(){
+     game("r");
+   })
+   paper_div.addEventListener('click',function(){
+    game("p");
+  })
+  scissors_div.addEventListener('click',function(){
+    game("s");
+  })
+}
+
